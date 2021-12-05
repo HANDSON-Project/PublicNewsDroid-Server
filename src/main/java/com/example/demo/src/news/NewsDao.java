338ac3da8cff7704cb13646eaee7af4a7733a153
Make-BeingSelf <sowhat443@gmail.com>
2021-12-05 23:46:40 +0900

    NewsDao Base Code

--- src/main/java/com/example/demo/src/news/NewsDao.java
+++ src/main/java/com/example/demo/src/news/NewsDao.java
@@ -1,4 +1,20 @@
 package com.example.demo.src.news;
 
+
+import com.example.demo.src.news.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Repository
 public class NewsDao {
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
 }
