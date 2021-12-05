49b921ccade8b3ccf8be22bbbc676952ada15117
Make-BeingSelf <sowhat443@gmail.com>
2021-12-05 23:39:05 +0900

    UserController Base Code

--- src/main/java/com/example/demo/src/user/UserController.java
+++ src/main/java/com/example/demo/src/user/UserController.java
@@ -1,4 +1,35 @@
 package com.example.demo.src.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.user.model.*;
+import com.example.demo.utils.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+import static com.example.demo.config.BaseResponseStatus.*;
+import static com.example.demo.utils.ValidationRegex.isRegexEmail;
+
+@RestController
+@RequestMapping("/app/users")
 public class UserController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private final UserProvider userProvider;
+    @Autowired
+    private final UserService userService;
+    @Autowired
+    private final JwtService jwtService;
+
+    public UserController(UserProvider userProvider, UserService userService, JwtService jwtService) {
+        this.userProvider = userProvider;
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
+    
 }
