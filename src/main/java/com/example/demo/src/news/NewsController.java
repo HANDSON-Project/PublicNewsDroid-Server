71dc1a505da1aec7f8fb7a9a5d59be139c3d3c13
jwMah <wigbuldok@naver.com>
2021-12-06 17:12:41 +0900

    news Controller CRU

--- src/main/java/com/example/demo/src/news/NewsController.java
+++ src/main/java/com/example/demo/src/news/NewsController.java
@@ -31,4 +31,81 @@ public class NewsController {
         this.jwtService = jwtService;
     }
 
+    // news 생성
+    @ResponseBody
+    @PostMapping("/{userIdx}")
+    public BaseResponse<PostNewsRes> createNews(@PathVariable("userIdx") int userIdx, @RequestBody PostNewsReq postNewsReq) {
+        try {
+            //jwt에서 idx 추출.
+            int userIdxByJwt = jwtService.getUserIdx();
+            //userIdx와 접근한 유저가 같은지 확인
+            if(userIdx!= userIdxByJwt){
+                return new BaseResponse<>(INVALID_USER_JWT);
+            }
+            if (postNewsReq.getTitle() == null) {
+                return new BaseResponse<>(POST_USERS_EMPTY_EMAIL);
+            }
+            if (postNewsReq.getContext() == null) {
+                return new BaseResponse<>(POST_USERS_EMPTY_EMAIL);
+            }
+            try {
+                PostNewsRes postNewsRes = newsService.createNews(postNewsReq);
+                return new BaseResponse<>(postNewsRes);
+            } catch (BaseException exception) {
+                return new BaseResponse<>((exception.getStatus()));
+            }
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+
+    }
+
+
+    // 모든 news들의  조회
+    // 특정 지역의 news들 조회
+    @ResponseBody
+    @GetMapping("")
+    public BaseResponse<List<GetNewsRes>> getNewses(@RequestParam(required = false) String location) {
+        try {
+            if (location == null) {
+                List<GetNewsRes> getNewsRes = newsProvider.getAllNews();
+                return new BaseResponse<>(getNewsRes);
+            }
+            List<GetNewsRes> getNewsRes = newsProvider.getNewsesByLocation(location);
+            return new BaseResponse<>(getNewsRes);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    // news 하나 조회
+    @ResponseBody
+    @GetMapping("/{newsIdx}")
+    public BaseResponse<GetNewsRes> getNews(@PathVariable("newsIdx") int newsIdx) {
+        try {
+            GetNewsRes getNewsRes = newsProvider.getNews(newsIdx);
+            return new BaseResponse<>(getNewsRes);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+
+    // news 정보 변경
+    @ResponseBody
+    @PatchMapping("/{userIdx}/{newsIdx}")
+    public BaseResponse<String> modifyUserName(@PathVariable("userIdx") int userIdx, @PathVariable("newsIdx") int newsIdx, @RequestBody PatchNewsReq patchUserReq) {
+        try {
+            int userIdxByJwt = jwtService.getUserIdx();
+            if(userIdx != userIdxByJwt){
+                return new BaseResponse<>(INVALID_USER_JWT);
+            }
+            News news = new News(userIdx, newsIdx,patchUserReq.getTitle(),patchUserReq.getContext(), patchUserReq.getImage(), patchUserReq.getLocation());
+            newsService.modifyNews(news);
+            String result = "뉴스정보가 수정되었습니다.";
+            return new BaseResponse<>(result);
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
 }
