e42467138bb58e1d325527bc7bfe8960ccbf8700
ChoiJaeEun <poungki0823@gmail.com>
2021-12-16 22:00:30 +0900

    특정 뉴스 댓글 조회 GetNewsCommentRes

---
+++ src/main/java/com/example/demo/src/reaction/model/GetNewsCommentRes.java
@@ -0,0 +1,12 @@
+package com.example.demo.src.reaction.model;
+
+import java.sql.Timestamp;
+
+public class GetNewCommentRes {
+    private int commentIdx;
+    private int userIdx;
+    private String nickname;
+    private String email;
+    private String context;
+    private Timestamp createdAt;
+}
