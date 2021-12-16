f862f41862dca9ae36afea016306410dc23a1854
ChoiJaeEun <poungki0823@gmail.com>
2021-12-16 22:36:06 +0900

    news 댓글 조회, 생성, 삭제 Dao

--- src/main/java/com/example/demo/src/reaction/ReactionDao.java
+++ src/main/java/com/example/demo/src/reaction/ReactionDao.java
@@ -1,9 +1,7 @@
 package com.example.demo.src.reaction;
 
 
-import com.example.demo.src.reaction.model.DeleteNewsCommentReq;
-import com.example.demo.src.reaction.model.GetNewsCommentRes;
-import com.example.demo.src.reaction.model.PostNewsCommentReq;
+import com.example.demo.src.reaction.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -19,18 +17,37 @@ public class ReactionDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+   
     // 뉴스 댓글 생성
     public void createNewsCommment(PostNewsCommentReq postNewsCommentReq) {
+        String createUserQuery = "insert into NewsComment (userIdx, newsIdx, context) VALUES (?,?,?)";
+        Object[] createUserParams = new Object[]{postNewsCommentReq.getUserIdx(), postNewsCommentReq.getNewsIdx(), postNewsCommentReq.getContent()};
+        this.jdbcTemplate.update(createUserQuery, createUserParams);
     }
 
     // 뉴스 댓글 조회
     // 해당 지역에 속한 유저들의 정보 조회 API
     public List<GetNewsCommentRes> getCommments(int newsIdx) {
-
-        return null;
+        String getCommmentsQuery = "select N.commentIdx, U.userIdx, U.nickname, U.email,N.context, N.createdAt\n" +
+                "from NewsComment N\n" +
+                "join User U on U.userIdx = N.userIdx\n" +
+                "where newsIdx =?";
+        int getCommmentsParams = newsIdx;
+        return this.jdbcTemplate.query(getCommmentsQuery,
+                (rs, rowNum) -> new GetNewsCommentRes(
+                        rs.getInt("commentIdx"),
+                        rs.getInt("userIdx"),
+                        rs.getString("nickname"),
+                        rs.getString("email"),
+                        rs.getString("context"),
+                        rs.getTimestamp("createdAt")),
+                getCommmentsParams);
     }
 
     // 뉴스 댓글 삭제
     public void deleteNewsCommment(DeleteNewsCommentReq deleteNewsCommentReq) {
+        String deleteNewsCommmentQuery = "delete from NewsComment where commeentIdx = ?";
+        Object[] deleteNewsCommmentParams = new Object[]{deleteNewsCommentReq.getCommentIdx()};
+        this.jdbcTemplate.update(deleteNewsCommmentQuery, deleteNewsCommmentParams);
     }
 }
