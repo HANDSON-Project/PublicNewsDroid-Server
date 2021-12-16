4ab6a5ff905206e4f73151b8add94a91510cdd0e
ChoiJaeEun <poungki0823@gmail.com>
2021-12-16 22:12:53 +0900

    뉴스 댓글 삭제 request DeleteNewsCommentReq 완성

--- src/main/java/com/example/demo/src/reaction/model/DeleteNewsCommentReq.java
+++ src/main/java/com/example/demo/src/reaction/model/DeleteNewsCommentReq.java
@@ -1,2 +1,12 @@
-package com.example.demo.src.reaction.model;public class DeleteNewsCommentReq {
+package com.example.demo.src.reaction.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DeleteNewsCommentReq {
+    private int userIdx;
+    private int commentIdx;
 }
