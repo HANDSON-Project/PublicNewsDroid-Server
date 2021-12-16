3fce64936393fd4e1665dd5ae53bdbce9a1d3d5c
ChoiJaeEun <poungki0823@gmail.com>
2021-12-16 22:43:09 +0900

    news 신고 Dao

--- src/main/java/com/example/demo/src/reaction/ReactionDao.java
+++ src/main/java/com/example/demo/src/reaction/ReactionDao.java
@@ -32,7 +32,7 @@ public class ReactionDao {
     }
 
     // 뉴스 댓글 생성
-    public void createNewsCommment(PostNewsCommentReq postNewsCommentReq) {
+    public void createNewsComment(PostNewsCommentReq postNewsCommentReq) {
         String createUserQuery = "insert into NewsComment (userIdx, newsIdx, context) VALUES (?,?,?)";
         Object[] createUserParams = new Object[]{postNewsCommentReq.getUserIdx(), postNewsCommentReq.getNewsIdx(), postNewsCommentReq.getContent()};
         this.jdbcTemplate.update(createUserQuery, createUserParams);
@@ -40,13 +40,13 @@ public class ReactionDao {
 
     // 뉴스 댓글 조회
     // 해당 지역에 속한 유저들의 정보 조회 API
-    public List<GetNewsCommentRes> getCommments(int newsIdx) {
-        String getCommmentsQuery = "select N.commentIdx, U.userIdx, U.nickname, U.email,N.context, N.createdAt\n" +
+    public List<GetNewsCommentRes> getComments(int newsIdx) {
+        String getCommentsQuery = "select N.commentIdx, U.userIdx, U.nickname, U.email,N.context, N.createdAt\n" +
                 "from NewsComment N\n" +
                 "join User U on U.userIdx = N.userIdx\n" +
                 "where newsIdx =?";
-        int getCommmentsParams = newsIdx;
-        return this.jdbcTemplate.query(getCommmentsQuery,
+        int getCommentsParams = newsIdx;
+        return this.jdbcTemplate.query(getCommentsQuery,
                 (rs, rowNum) -> new GetNewsCommentRes(
                         rs.getInt("commentIdx"),
                         rs.getInt("userIdx"),
@@ -54,17 +54,20 @@ public class ReactionDao {
                         rs.getString("email"),
                         rs.getString("context"),
                         rs.getTimestamp("createdAt")),
-                getCommmentsParams);
+                getCommentsParams);
     }
 
     // 뉴스 댓글 삭제
-    public void deleteNewsCommment(DeleteNewsCommentReq deleteNewsCommentReq) {
-        String deleteNewsCommmentQuery = "delete from NewsComment where commeentIdx = ?";
-        Object[] deleteNewsCommmentParams = new Object[]{deleteNewsCommentReq.getCommentIdx()};
-        this.jdbcTemplate.update(deleteNewsCommmentQuery, deleteNewsCommmentParams);
+    public void deleteNewsComment(DeleteNewsCommentReq deleteNewsCommentReq) {
+        String deleteNewsCommentQuery = "delete from NewsComment where commeentIdx = ?";
+        Object[] deleteNewsCommentParams = new Object[]{deleteNewsCommentReq.getCommentIdx()};
+        this.jdbcTemplate.update(deleteNewsCommentQuery, deleteNewsCommentParams);
     }
 
     // 뉴스 신고
     public void createNewsReport(PostNewsReportReq postNewsReportReq) {
+        String createNewsReportQuery = "insert into NewsReport (userIdx, newsIdx) VALUES (?,?)";
+        Object[] createNewsReportParams = new Object[]{postNewsReportReq.getUserIdx(), postNewsReportReq.getNewsIdx()};
+        this.jdbcTemplate.update(createNewsReportQuery, createNewsReportParams);
     }
 }
